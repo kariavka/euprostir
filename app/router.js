0ce816c2b3c078bb12cef6b4b148c221b28fed4e
Artem Kariavka <artem@kariavka.com>
2018-03-25 12:18:18 +0300

    [+] Added main routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,16 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('stories', function() {
+    this.route('item');
+  });
+  this.route('practicies', function() {
+    this.route('item');
+  });
+  this.route('opportunities', function() {
+    this.route('item');
+  });
 });
 
 export default Router;
