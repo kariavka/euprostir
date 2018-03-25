28e63e8dbd780711076de3b373d77e3257e3ba30
Artem Kariavka <artem@kariavka.com>
2018-03-25 17:44:05 +0300

    [+] Added Opportunities block to Homepage

--- app/routes/opportunities/item.js
+++ app/routes/opportunities/item.js
@@ -1,4 +1,22 @@
 import Route from '@ember/routing/route';
+import {inject} from '@ember/service';
+import {get, set} from '@ember/object';
+import {reads} from '@ember/object/computed';
+import {hash} from 'rsvp';
 
 export default Route.extend({
+  // Services
+  store: inject(),
+  headData: inject(),
+
+  // Title
+  title: reads('model.item.title'),
+
+  // Model
+  model(params) {
+    const store = get(this, 'store');
+    return hash({
+      item: store.findRecord('post', params.id),
+    });
+  },
 });
