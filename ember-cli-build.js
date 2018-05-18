6549f7916daabed037611b06db31280c44e4fd66
Artem Kariavka <artem@kariavka.com>
2018-05-18 17:56:55 +0300

    [*] Upgrade to Ember 3.1.0

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,9 +2,19 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    'ember-bootstrap': {
+      bootstrapVersion: 4,
+      importBootstrapFont: false,
+      importBootstrapCSS: false
+    },
+
+    'ember-font-awesome': {
+      useScss: true,
+      includeFontFiles: true,
+      includeFontAwesomeAssets: true
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
