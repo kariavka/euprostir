8012a9b321f2d37cf4f56d64ee3d11b139e18472
Artem Kariavka <artem@kariavka.com>
2018-05-21 22:39:57 +0300

    [+] Added Scroll Reset after Route transition

--- app/components/common/common-header.js
+++ app/components/common/common-header.js
@@ -6,6 +6,22 @@ export default Component.extend({
   // Services
   media: inject(),
 
+  // Hooks
+  didRender() {
+    this._super(...arguments);
+    let scrollStart = 160;
+    $(window).scroll(function () {
+      let scroll = $(window).scrollTop();
+      if (scroll >= scrollStart) {
+        $('.menu_fixed').addClass('show');
+      }
+      else {
+        $('.menu_fixed').removeClass('show');
+      }
+    });
+
+  },
+
   // Actions
   actions: {
     onMenuMobileClick() {
