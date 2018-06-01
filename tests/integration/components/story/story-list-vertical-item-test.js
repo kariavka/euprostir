bc50d66ce6082e0139fa6b800a855b19eb3fdfe4
Artem Kariavka <artem@kariavka.com>
2018-06-01 17:25:36 +0300

    [*] Updated Story Item UI

--- tests/integration/components/story/story-list-vertical-item-test.js
+++ tests/integration/components/story/story-list-vertical-item-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | story/story-popular-list-item', function(hooks) {
+module('Integration | Component | story/story-list-vertical-item', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{story/story-popular-list-item}}`);
+    await render(hbs`{{story/story-list-vertical-item}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      {{#story/story-popular-list-item}}
+      {{#story/story-list-vertical-item}}
         template block text
-      {{/story/story-popular-list-item}}
+      {{/story/story-list-vertical-item}}
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
