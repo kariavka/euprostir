25578de3bb89821a3042f7ec444fac01d4400ec6
Artem Kariavka <artem@kariavka.com>
2018-06-05 13:52:10 +0300

    [*] Updated Opportunity Lists UI

--- app/routes/opportunities/index.js
+++ app/routes/opportunities/index.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 import {inject} from '@ember/service';
-import {get} from '@ember/object';
-//import {reads} from '@ember/object/computed';
+import {get, set} from '@ember/object';
+import {reads} from '@ember/object/computed';
 import {hash} from 'rsvp';
 import getLira from 'euprostir/utils/get-lira';
 
@@ -13,16 +13,40 @@ export default Route.extend({
   // Title
   title: 'Можливості - Європейський простір',
 
+  // Params
+  queryParams: {
+    f: {refreshModel: true}
+  },
+
   // Model
-  model() {
+  model(params) {
     const store = get(this, 'store');
+    let lira = getLira('properties');
+    let liraliraWithFilter = getLira('properties');
+    let filter = params.f;
+
+    if (filter) {
+      liraWithFilter = lira + ',' + filter;
+    }
+
     return hash({
       items: store.query('post', {
         page: 1,
-        per_page: 9,
-        lira: getLira('opportunities'),
-        sort: '-created',
+        per_page: 4,
+        lira: lira
+      }),
+      popular: store.query('post', {
+        page: 1,
+        per_page: 3,
+        lira: lira,
+        sort: '-views'
       }),
     });
+  },
+
+  setupController: function (controller, model) {
+    controller.set('showDropdown', false);
+    controller.set('model', model);
+    controller.set('items', model.items);
   },
 });
