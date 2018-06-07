fa6efb7f4bfe74d1f8a5d5c632d6d9eebf3c52d8
Artem Kariavka <artem@kariavka.com>
2018-06-07 18:15:45 +0300

    [+] Added Courses Page UI

--- app/routes/courses/index.js
+++ app/routes/courses/index.js
@@ -1,4 +1,50 @@
 import Route from '@ember/routing/route';
+import {inject} from '@ember/service';
+import {get, set} from '@ember/object';
+import {reads} from '@ember/object/computed';
+import {hash} from 'rsvp';
+import getLira from 'euprostir/utils/get-lira';
 
 export default Route.extend({
+  // Services
+  store: inject(),
+
+  // Title
+  title: 'Онлайн курси - Європейський простір',
+
+  // Params
+  queryParams: {
+    f: {refreshModel: true}
+  },
+
+  // Model
+  model(params) {
+    const store = get(this, 'store');
+    let lira = getLira('courses');
+    let filter = params.f;
+
+    if (filter) {
+      lira = lira + ',' + filter;
+    }
+
+    return hash({
+      items: store.query('post', {
+        page: 1,
+        per_page: 4,
+        lira: lira
+      }),
+      popular: store.query('post', {
+        page: 1,
+        per_page: 3,
+        lira: lira,
+        sort: '-views'
+      }),
+    });
+  },
+
+  setupController: function (controller, model) {
+    controller.set('showDropdown', false);
+    controller.set('model', model);
+    controller.set('items', model.items);
+  },
 });
