530989247ef883dd5e61e1888760becbfb3a2adb
Artem Kariavka <artem@kariavka.com>
2018-10-11 00:43:35 +0300

    [*] Updated Events Filters UI

--- app/components/event/event-filter.js
+++ app/components/event/event-filter.js
@@ -1,9 +1,21 @@
 import Component from '@ember/component';
-import {set} from '@ember/object';
+import {get, set, computed} from '@ember/object';
 
 export default Component.extend({
   // Flags
   showDropdown: false,
+
+  // Attributes
+  filters: null,
+  value: null,
+
+  // Computed
+  label: computed('filters.*', 'value', function () {
+    const filters = get(this, 'filters');
+    const value = get(this, 'value');
+    const filtered = filters.filter((item) => item.value === value);
+    return (filtered.length > 0) ? filtered[0].name : null;
+  }),
 
   // Actions
   actions: {
--- app/components/event/event-filters.js
+++ app/components/event/event-filters.js
@@ -10,7 +10,29 @@ export default Component.extend({
   init() {
     this._super(...arguments);
     const subjectFilters = config.neuronet.uk.filters.events;
+    const monthFilters = [
+      {value: '1', name: 'Січень'},
+      {value: '2', name: 'Лютий'},
+      {value: '3', name: 'Березнь'},
+      {value: '4', name: 'Квітень'},
+      {value: '5', name: 'Травень'},
+      {value: '6', name: 'Червень'},
+      {value: '7', name: 'Липень'},
+      {value: '8', name: 'Серпень'},
+      {value: '9', name: 'Вересень'},
+      {value: '10', name: 'Жовтень'},
+      {value: '11', name: 'Листопад'},
+      {value: '12', name: 'Грудень'},
+    ];
+    const yearFilters = [
+      {value: '2018', name: '2018'},
+      {value: '2019', name: '2019'},
+    ];
+
+
     set(this, 'subjectFilters', subjectFilters);
+    set(this, 'monthFilters', monthFilters);
+    set(this, 'yearFilters', yearFilters);
   },
 
   // Computed
