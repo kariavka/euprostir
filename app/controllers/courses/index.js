38a15f91cc4cfbd56212f9006c3aef1cd7241221
kariavka <artem@kariavka.com>
2019-06-08 00:05:03 +0300

    [+] Added Load More functionality to Courses Index UI

--- app/controllers/courses/index.js
+++ app/controllers/courses/index.js
@@ -1,8 +1,3 @@
 import Controller from '@ember/controller';
-import {inject as service} from '@ember/service';
-import {get, set, computed} from '@ember/object';
-import {reads} from '@ember/object/computed';
-import {A} from '@ember/array';
-import config from 'euprostir/config/environment';
 
 export default Controller.extend({});
