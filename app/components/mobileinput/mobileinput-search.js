67d1a4c230e238ca37bd4342679ccf1574a9d8e3
andrey <zoizoizoi@i.ua>
2020-09-28 00:27:33 +0300

    mobile search fixed

---
+++ app/components/mobileinput/mobileinput-search.js
@@ -0,0 +1,61 @@
+import Component from '@ember/component';
+import {inject as service} from '@ember/service';
+import {get, set} from '@ember/object';
+import config from 'euprostir/config/environment';
+
+export default Component.extend({
+  // Store
+  store: service(),
+  i18n: service(),
+
+  // Flags
+  isSearchMade: false,
+
+  // Actions
+  actions: {
+    search() {
+      const store = get(this, 'store');
+      const q = get(this, 'q');
+      const locale = get(this, 'i18n.locale');
+
+      store.query('post', {
+        q: q,
+        page: 1,
+        per_page: 7,
+        lira: config.neuronet[locale].stories,
+        'filter[site]': config.neuronet.site,
+        'filter[language]': locale,
+      }).then((results) => {
+        set(this, 'isSearchMade', true);
+        set(this, 'stories', results);
+        return results;
+      });
+
+      store.query('post', {
+        q: q,
+        page: 1,
+        per_page: 7,
+        lira: config.neuronet[locale].practices,
+        'filter[site]': config.neuronet.site,
+        'filter[language]': locale,
+      }).then((results) => {
+        set(this, 'isSearchMade', true);
+        set(this, 'practices', results);
+        return results;
+      });
+
+      store.query('post', {
+        q: q,
+        page: 1,
+        per_page: 7,
+        lira: config.neuronet[locale].opportunities,
+        'filter[site]': config.neuronet.site,
+        'filter[language]': locale,
+      }).then((results) => {
+        set(this, 'isSearchMade', true);
+        set(this, 'opportunities', results);
+        return results;
+      });
+    },
+  },
+});
